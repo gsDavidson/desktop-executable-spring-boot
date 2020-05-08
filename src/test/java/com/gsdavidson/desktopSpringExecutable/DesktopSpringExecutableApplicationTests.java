cf0d475a72853885bd63f83126feb78072b6365a
Grant Davidson <gdavidson606@gmail.com>
2020-05-08 15:11:06 -0500

    adding new new files

---
+++ src/test/java/com/gsdavidson/desktopSpringExecutable/DesktopSpringExecutableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsdavidson.desktopSpringExecutable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesktopSpringExecutableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
