0fe7b25e0e14817528dcd0b92bd68526fda5d68b
Grant Davidson <gdavidson606@gmail.com>
2020-05-31 14:49:17 -0500

    Changing to use a file write as validation over popup

--- src/main/java/com/gsdavidson/desktopSpringExecutable/DesktopSpringExecutableApplication.java
+++ src/main/java/com/gsdavidson/desktopSpringExecutable/DesktopSpringExecutableApplication.java
@@ -1,13 +1,27 @@
 package com.gsdavidson.desktopSpringExecutable;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import javax.swing.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
 
 @SpringBootApplication
 public class DesktopSpringExecutableApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DesktopSpringExecutableApplication.class, args);
+
+	public static void main(String[] args) throws IOException {
+		String fileSuccessText = "Successfully created this file at: " + new Date();
+
+		BufferedWriter writer = new BufferedWriter(new FileWriter("RunnableJavaFile.txt"));
+		writer.write(fileSuccessText);
+		writer.close();
 	}
 
 }
